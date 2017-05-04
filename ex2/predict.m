bc18a95cec06db9ee76fe82e00cfe13aff5dbd43
Gerald Yang <gerald.yang@canonical.com>
2017-05-04 22:08:39 +0800

    Complete sigmoid and predict functions.

--- ex2/predict.m
+++ ex2/predict.m
@@ -15,10 +15,10 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+res = 1 ./ (1 + exp(-(X * theta)));
 
-
-
-
+pos = find(res >= 0.5);
+p(pos) = 1;
 
 
 % =========================================================================
