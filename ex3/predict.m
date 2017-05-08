eae596bac065e4a51c3c21b647998d8139a0362a
Gerald Yang <gerald.yang@canonical.com>
2017-05-08 23:52:36 +0800

    Complete neural network prediction.

--- ex3/predict.m
+++ ex3/predict.m
@@ -21,12 +21,17 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m, 1), X];
+size(X)
+size(Theta1)
+size(Theta2)
+size(p)
 
+layer2 = sigmoid(X * Theta1');
+layer2 = [ones(size(layer2(:,1)), 1) layer2];
 
-
-
-
-
+layer3 = sigmoid(layer2 * Theta2');
+[max_p, p] = max(layer3, [], 2);
 
 
 % =========================================================================
