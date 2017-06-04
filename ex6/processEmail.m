14d690a282adb349a9492589f362db4be695861e
Gerald Yang <gerald.yang@canonical.com>
2017-06-04 16:00:19 +0800

    Complete email spam classifier.

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -97,14 +97,10 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    index = find(strcmp(str, vocabList), 1);
+    if index != 0
+	    word_indices = [word_indices ; index];
+    endif
 
     % =============================================================
 
